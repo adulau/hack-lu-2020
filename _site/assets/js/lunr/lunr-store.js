133cd25211f159b8ff0f478568986f9c67fb952e
Alexandre Dulaunoy <a@foo.be>
2020-10-22 20:52:22 +0200

    updated

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,21 +1,6 @@
 var store = [{
-        "title": "Call for papers is now open for hack.lu 2019",
-        "excerpt":"Hack.lu 2019 Call for Papers, Presentations and Workshops Sometimes it's the detours which turn out to be the fruitful ideas. Roger Penrose The purpose of the hack.lu convention is to give an open and freeplayground where people can discuss the implication of newtechnologies in society. hack.lu is a balanced mix...","categories": ["blog"],
+        "title": "Call for papers is now open for hack.lu 2020",
+        "excerpt":"Hack lu 2020 CfP Submit a proposal, get published in a book Due to the current situation, and as it is extremely unlikely there will be a vaccine, or a cure, to coronavirus before 2021, we decided to come with another plan for the conference. By October, if you’re into...","categories": ["blog"],
         "tags": [],
-        "url": "/blog/hack.lu-2019-call-for-papers/",
-        "teaser":null},{
-        "title": "Call for Failure (CfF 0x0) - Over Fail the untold truth behind the magic of cybersecurity",
-        "excerpt":"Hack.lu 2019 - Call for Failure (Cff 0x) is open Over Fail: the untold truth behind the magic of cybersecurity. During hack.lu 2019, we have a mini-conferencewithin hack.lu conference to proudly share your failures in cybersecurity. 10-minute talks during hack.lu to present (the/your) biggest failure in cybersecurity you’ve ever experienced...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/hack.lu-Call-For-Failure/",
-        "teaser":null},{
-        "title": "Job Fair hack.lu 2019",
-        "excerpt":"Job Fair @ hack.lu 2019 As Hack.lu is the biggest and most established technical information security conference in the benelux region,it attracts a lot of highly technical attendees and some of them are looking for new challenges. This is why we decided to give the opportunity to companies looking for...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/hack.lu-2019-jobfair/",
-        "teaser":null},{
-        "title": "Agenda hack.lu 2019",
-        "excerpt":"Agenda hack.lu 2019 The Agenda of hack.lu 2019 is now available, please keep an eye on it in case there are last minute changes.You can also get it in ICS format. And note that there are a few tickets left if you want to attend the conference. ","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/hack.lu-2019-Agenda/",
+        "url": "/blog/hack.lu-2020-call-for-papers/",
         "teaser":null}]
--- _site/build.sh
+++ _site/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bundle exec jekyll build
-rsync -v -4 -rz --checksum  _site/ adulau@kb.quuxlabs.com:/home/adulau/website/hacklu2019/
+rsync -v -4 -rz --checksum  _site/ adulau@kb.quuxlabs.com:/home/adulau/website/hacklu2020/
